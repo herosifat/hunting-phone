3094f32a9686fb5d878c6e24776a6f50b80b378b
Your Namesifathero <sifathero466@gmail.com>
2024-03-28 22:49:10 +0600

    modal add

--- js/phone.js
+++ js/phone.js
@@ -31,8 +31,8 @@ phones.forEach(phone=> {
   <div class="card-body">
     <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-       <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+       <div class="card-actions justify-center">
+      <button onclick="showAllDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
       </div>
         </div>
   `;
@@ -68,6 +68,28 @@ const loadingSpenner =(isLoading) =>{
 
 // show All button
 
-const ShawAll= ()=>[
+const ShawAll= ()=>{
     evenHandler2(true)
-]
+}
+
+// show Details Button
+const showAllDetails = async (id) =>{
+  console.log('click on show Details', id);
+  // loading single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+ const data = await res.json();
+const phone =data.data
+ displayModal(phone)
+}
+// modal
+const displayModal = (phone) =>{
+  console.log(phone);
+  const phoneName =document.getElementById('show-ph-details');
+  const phonePicture = document.getElementById('picture-phone')
+  phonePicture.innerHTML=`
+  <img src="${phone.image}" alt="" />
+  `;
+  phoneName.innerText =phone.name;
+  // show display 
+  Shaw_modal_5.showModal();
+}
